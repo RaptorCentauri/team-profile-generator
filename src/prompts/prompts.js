1ddbe49bb3fc124adb7f3fa1f7124b1737cb3b96
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-07-11 20:09:07 +0000

    prompts display role name

--- src/prompts/prompts.js
+++ src/prompts/prompts.js
@@ -2,42 +2,42 @@ const teamGeneratorPrompts = {
 	
 	employee: [
 		{
-			name:`name`,
-			type:`input`,
-			message: `What is the employee's name?`
+			name:`role`,
+			type:`list`,
+			message: `What type of employee are you entering?`,
+			choices: [`Manager`, `Engineer`, `Intern`]
 		},
 		{
-			name:`id`,
+			name:`name`,
 			type:`input`,
-			message: (answers) => `What is ${answers.name}'s id number?`
+			message: (answers) => `What is the ${answers.role}'s name?`
 		},
 		{
 			name:`email`,
 			type:`input`,
-			message: (answers) => `What is ${answers.name}'s email?`
+			message: (answers) => `What is the ${answers.role}'s email?`
 		},
 		{
-			name:`role`,
-			type:`list`,
-			message: (answers) => `What is ${answers.name}'s role?`,
-			choices: [`Manager`, `Engineer`, `Intern`]
+			name:`id`,
+			type:`input`,
+			message: (answers) => `Please enter an id for the ${answers.role}`
 		},
 		{
 			type: `input`,
 			name: `officeNumber`,
-			message: (answers) => `What is ${answers.name}'s office number?`,
+			message: (answers) => `What is the ${answers.role}'s office number?`,
 			when: (answers) => answers.role === `Manager`
 		},
 		{
 			type: `input`,
 			name: `github`,
-			message: (answers) => `What is ${answers.name}'s github profile?`,
+			message: (answers) => `What is the ${answers.role}'s github profile?`,
 			when: (answers) => answers.role === `Engineer`
 		},
 		{
 			type: `input`,
 			name: `school`,
-			message: (answers) => `What is ${answers.name}'s school?`,
+			message: (answers) => `What is the ${answers.role}'s school?`,
 			when: (answers) => answers.role === `Intern`
 		}
 	],
